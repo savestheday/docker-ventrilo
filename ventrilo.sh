3b84616f45cf32602a4fec1e90ce9080f1143973
savestheday <sparklemotion@gmail.com>
2014-12-24 21:32:11 -0500

    Update ventrilo.sh

--- ventrilo.sh
+++ ventrilo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec /opt/ventsrv/ventrilo_srv -f/opt/ventsrv/ventrilo_srv >>/var/log/ventrilo.log 2>&1
+exec /opt/ventsrv/ventrilo_srv -f/opt/ventsrv/cfg/ventrilo_srv >>/var/log/ventrilo.log 2>&1
