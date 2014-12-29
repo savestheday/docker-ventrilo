9860b5af4f59dc9dcb7231a00b117782a880b5e1
savestheday <sparklemotion@gmail.com>
2014-12-28 21:58:26 -0500

    Update ventrilo.sh

--- ventrilo.sh
+++ ventrilo.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 # Check if existing config files exist, if so will use it.
-  
-  if [ -f /config/ventrilo_srv.ini ]; then
-    echo "Previous config exists, copying.."  
-    cp /config/ventrilo_srv.ini /opt/ventsrv/ventrilo_srv.ini
-    cp /config/ventrilo_srv.usr /opt/ventsrv/ventrilo_srv.usr
-    cp /config/ventrilo_srv.chn /opt/ventsrv/ventrilo_srv.chn
-  fi
-  
+
+cp /config/ventrilo_srv.ini /opt/ventsrv/ventrilo_srv.ini
+cp /config/ventrilo_srv.usr /opt/ventsrv/ventrilo_srv.usr
+cp /config/ventrilo_srv.chn /opt/ventsrv/ventrilo_srv.chn
+
 exec /opt/ventsrv/ventrilo_srv -f/opt/ventsrv/ventrilo_srv >>/var/log/ventrilo.log 2>&1
