56325966948fa032586820fa35474d2538bf56ad
savestheday <sparklemotion@gmail.com>
2014-12-28 22:10:05 -0500

    Update ventrilo.sh

--- ventrilo.sh
+++ ventrilo.sh
@@ -5,4 +5,5 @@ cp /config/ventrilo_srv.ini /opt/ventsrv/ventrilo_srv.ini
 cp /config/ventrilo_srv.usr /opt/ventsrv/ventrilo_srv.usr
 cp /config/ventrilo_srv.chn /opt/ventsrv/ventrilo_srv.chn
 
-exec /opt/ventsrv/ventrilo_srv -f/opt/ventsrv/ventrilo_srv >>/var/log/ventrilo.log 2>&1
+#exec /opt/ventsrv/ventrilo_srv -f/opt/ventsrv/ventrilo_srv >>/var/log/ventrilo.log 2>&1
+exec /opt/ventsrv/ventrilo_srv -d >>/var/log/ventrilo.log 2>&1
